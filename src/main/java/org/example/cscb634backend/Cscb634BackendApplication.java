faa09e2954778c6f77ca6ba1a274eb6a60084f5a
cakimaki <dani.tsakovski3@gmail.com>
2024-04-03 22:14:48 +0300

    creation

---
+++ src/main/java/org/example/cscb634backend/Cscb634BackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cscb634backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cscb634BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cscb634BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/cscb634backend/Cscb634BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cscb634backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cscb634BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
