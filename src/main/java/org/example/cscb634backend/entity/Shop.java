b8c0a3d0b755af08e6187da9a6d469d592e89adc
cakimaki <dani.tsakovski3@gmail.com>
2024-04-04 13:50:38 +0300

    Create Shop.java
    Shop is done, the properties should be done and the rest of the entities

---
+++ src/main/java/org/example/cscb634backend/entity/Shop.java
@@ -0,0 +1,51 @@
+package org.example.cscb634backend.entity;
+
+import jakarta.persistence.*;
+import org.apache.logging.log4j.message.AsynchronouslyFormattable;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name= "shop")
+public class Shop {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "address")
+	private String address;
+	
+	public Shop() {
+	}
+	
+	public Shop(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
