76b322a7082699a728e5cffedeb5200c54494b1c
cakimaki <dani.tsakovski3@gmail.com>
2024-04-14 21:56:15 +0300

    samplecontroller,supplierEntity

---
+++ src/main/java/org/example/cscb634backend/controller/SampleController.java
@@ -0,0 +1,13 @@
+package org.example.cscb634backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping("/")
+	public String index(){
+		return "Connection has been established";
+	}
+}
--- src/main/java/org/example/cscb634backend/entity/product/Supplier.java
+++ src/main/java/org/example/cscb634backend/entity/product/Supplier.java
@@ -1,4 +1,33 @@
 package org.example.cscb634backend.entity.product;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "supplier")
 public class Supplier {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	
+	
+	public Supplier() {
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
